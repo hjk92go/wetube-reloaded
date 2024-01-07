25230172a821588b992baffe367b7065c4db39b8
Hyeonju Kim <hjk92go@gmail.com>
2024-01-07 16:21:10 +0900

    2.2 Understanding Dependencies

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 console.log("hello nodeJS");
+/*
+ * dependencies는 프로젝트를 구동시키는데 필요한 모듈들을 의미함
+ * package.json에 dependencies에 해당되는 모듈을
+ * npm i 로 파일을 전부 설치 가능(즉, package.json파일은 반드시 필요)
+ *
+ * 굳이 용량 큰 모듈파일까지 깃이든 어디든 올릴필요없음
+ *
+ * .gitignore 파일을 만들어
+ * /node_modules 파일을 추가하면 깃에 폴더를 올리지 않게 됨
+ *
+ */
