24c426f5956b82f2c6547c8aa04a54daeeb45e53
Hyeonju Kim <hjk92go@gmail.com>
2024-01-07 17:54:04 +0900

    3.1 GET Requests

--- src/server.js
+++ src/server.js
@@ -1,25 +1,25 @@
-/**
- * 서버란 항상 켜져있는 컴퓨터
- * 사람들의 요청을 기다리고 답해준다
- */
-
 import express from "express";
-
-const PORT = 4000;
 
 //express app 생성
 const app = express();
+const PORT = 4000;
 
 const handleListening = () => {
-  console.log(`Server listening on port http://localhost:${PORT} ✈️✈️`);
+  console.log(`Server listening on port ✅http://localhost:${PORT} ✈️✈️`);
 };
 
-//서버가 사람들이 뭔가를 요청할 때까지 기다리게 하기
-//어떤포트를 기다리는지 포트를 쓰고, callback을 써준다
-app.listen(PORT, handleListening);
-
 /**
- * 아래와 같이 사용도 가능
- * app.listen(PORT, () => console.log(`Server listening on port http://localhost:${PORT} ✈️✈️`));
+ *
+ * 3.1 서버가 request에 respond하는법
+ *
+ * 아무런 설정을 하지않은상태에서 서버만 열면
+ * Cannot GET / 라고 출력되는데 여기서 '/'의 의미는 서버의 root, 혹은 첫번째페이지를 의미
+ *
+ * ex)google.com === google.com/
+ *
+ * GET은 HTTP method => 쉽게 페이지를 가져와! 라고 생각하면된다
+ * http는 서버끼리 의사소통하는방법
  *
  */
+
+app.listen(PORT, handleListening);
