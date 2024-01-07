5f0ba01595b15c7cdb24a1bf8941e633d99eea90
Hyeonju Kim <hjk92go@gmail.com>
2024-01-07 16:46:12 +0900

    2.3 bable install && understanding

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-console.log("hello nodeJS");
-/*
- * dependencies는 프로젝트를 구동시키는데 필요한 모듈들을 의미함
- * package.json에 dependencies에 해당되는 모듈을
- * npm i 로 파일을 전부 설치 가능(즉, package.json파일은 반드시 필요)
+/**
  *
- * 굳이 용량 큰 모듈파일까지 깃이든 어디든 올릴필요없음
+ * bable 은 자바스크립트 컴파일러
+ * 바벨은 우리가 작성한 최신 JS로 컴파일해준다
+ * => nodeJS가 JS를 문제없이 이해하도록 변환해줌
  *
- * .gitignore 파일을 만들어
- * /node_modules 파일을 추가하면 깃에 폴더를 올리지 않게 됨
+ *
+ *
+ * dependencies는 프로젝트를 실행하기 위한걸 말하고,
+ * devDependencies은 개발자(사람)에게 필요한걸 나타낸다
+ * --save--dev 로 설치하면 package.json의 devDependencies에 표시된다
+ *
+ *
+ * https://babeljs.io/docs/ 참고(사용하는 종류에 따라 설치하는 종류는 달라짐)
+ * npm install --save-dev @babel/core 설치
+ * 바벨을 설치하고 babel.config.json파일을 만든뒤 바벨 공홈에 있는 내용을 복붙해준다
+ * npm install @babel/preset-env --save-dev 설치해준다
  *
  */
