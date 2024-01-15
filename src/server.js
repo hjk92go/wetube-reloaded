4a665e6c30bf70833e403aafc5e714a7532fb3fd
hjk92go <113073075+hjk92go@users.noreply.github.com>
2024-01-15 11:14:59 +0900

    3.4 Recap

--- src/server.js
+++ src/server.js
@@ -4,43 +4,27 @@ const PORT = 4000;
 const app = express();
 
 /**
- * 브라우저가 req를 보내서 우리가 req를 받는다면,  res를 return 해줘야함
- * res.end하면 서버는 req를 종료 더이상 무한로딩X
- * res.end()는 req를 종료시키는 여러 방법중 하나
- * res.send()를 이용해도 req를 종료시킬수 잇음
+ *  3.4 Recap
+ * request는 응답이 필수 => 응답x => 브라우저는 하염없이 기다림
  *
- * 정리하면
- * 우리는 브라우저가 보낸 request를 받고 ~~페이지를 가져달라는 request를 받으면
- * 우리는 응답해주는것이 브라우저와의 상호작용이다.
- *
+ * status code로 응답하거나,,, html로 하거나.. end하거나 꼭 응답이 필요함
  */
 
+//html도 보낼수잇고
 const handleListeningHome = (req, res) => {
-  return res.send("I still love you 😭");
+  return res.send("<h1>I still love you 😭</h1>");
 };
-app.get("/", handleListeningHome);
 
-/**
- * 서버는 request를 받고, listening하지만 응답은 안해준다.
- *
- * JS에서는 eventListener에는 handler가 있고, handler에는 event가 있음
- * Express에선, route handler에는 event는 없지만 object 두개가 잇음
- *
- * request(req)와 response(res) => 이름은 아무거나 상관없음.
- * 핵심은 첫번째 argument는 request object 이고, 두번째는 response object
- *
- * => 즉 home으로 get request가 오면, express는 handleHome에다가
- * handleHome({...},{...}) 뒤에서 일어남(req object를 제공한단말)
- *
- */
-
+//json도보낼수잇음(react.js나 api만드는중이라면 유용)
 const handleLogin = (req, res) => {
-  return res.send("Login Here👍");
+  return res.send({ message: "Login Here👍" });
 };
 
+app.get("/", handleListeningHome);
 app.get("/login", handleLogin);
 
 const handleListening = () => {
   console.log(`✅ Server listening on port http://localhost:${PORT} ✈️✈️`);
 };
+
 app.listen(PORT, handleListening);
