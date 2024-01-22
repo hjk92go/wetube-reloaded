ecdf29494884d86de135d0740c334491d0c49811
Hyeonju Kim <hjk92go@gmail.com>
2024-01-22 20:58:02 +0900

    4.8 URL Parameters(part_2)

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -4,24 +4,29 @@ import { see, upload, deleteVideo, edit } from "../controllers/videoController";
 const videoRouter = express.Router();
 //1
 videoRouter.get("/upload", upload);
-//2
-videoRouter.get("/:id", see);
-videoRouter.get("/:id/edit", edit);
-videoRouter.get("/:id/delete", deleteVideo);
+
+//2_파라미터에 숫자만 들어갈수 있게 정규식 사용 => 이렇게 사용하면 1번의 upload는 어떤위치에 와도 상관없다
+videoRouter.get("/:id(\\d+)", see);
+videoRouter.get("/:id(\\d+)/edit", edit);
+videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 
 /**
- * 4.7 URL Parameters(part_1)
+ * 4.8 URL Parameters(part_2)
  *
- * " /:id "에서 :가 의미하는 것?
- * 반드시 :id 일 필요는 없다 :potato 등등 중요한건 ":"를 넣는것
- * id는 변수명일뿐
- * :id은 파라미터라고 불리는데 이걸로  URL안에 변수를 넣을수 있게 해준다.
- * 파라미터가 없다면 모든 영상마다 라우터를 새로 만들어야 했을것이다.
+ * id를 숫자만 인정하고 싶다 => 정규식 사용(공문참조)
+ * 정규식은 모든 프로그래밍 언어에 존재
  *
- * 변수명은 어떠한것이 되어도 상관없지만 " : "" 은 꼭 잇어야한다.
+ * Routing
+ * https://expressjs.com/ko/guide/routing.html
  *
- * upload가 :id위로 쓴 이유 => (1,2번)순서를 바꾼다면 upload가 id자리에 있어서id 취급됨
- * 그래서 가장 위로 옮기면 upload 변수 취급X -> URL취급함
+ * 정규표현식 테스트 사이트
+ * https://www.regexpal.com
+ *
+ * \w+: 모든 문자, 숫자 선택
+ * \d+: 모든 숫자 선택
+ *
+ * ex) /(nico\w+)/니꼬로 시작하는 모든 문자를 선택하세요
+ * ex) /(\d+)/ 숫자로 시작하는 문자를 선택 => Js의 경우 //두개 써줘야함 (\\d+)
  *
  *
  */
