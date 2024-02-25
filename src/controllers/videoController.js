b2395374f17e1aae557b758f11bbf8be14a4749a
Hyeonju Kim <hjk92go@gmail.com>
2024-02-25 17:43:28 +0900

    #5.1 Configuring Pug

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,5 @@
 //global router
-export const trending = (req, res) =>
-  res.send(`<!DOCTYPE html><html lang='ko'><head><title>Wetube</title></head>
-<body><h1>HOME</h1></body></html><footer>&copy; 2021 Wetube- All rights reserved</footer>`);
+export const trending = (req, res) => res.render("home"); //res.render("view name") => 이렇게 하면 home.pug가 랜더링 된다.
 export const search = (req, res) => res.send("search");
 
 //video router
@@ -20,6 +18,3 @@ export const deleteVideo = (req, res) => {
   console.log(req.params);
   return res.send("delete");
 };
-
-//export default는 한개만
-//const앞 export를 붙여주면 한파일이 여러개를 익스포트 할수잇다
--- src/server.js
+++ src/server.js
@@ -7,10 +7,14 @@ import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
 
 const PORT = 4000;
+
+console.log("process.cwd : ", process.cwd()); //현재 작업디렉토리를 의미함
+
 const app = express();
 const logger = morgan("dev");
-app.use(logger); //app.use(morgan("dev"))라고 사용해도 같음
 
+app.set("view engine", "pug"); //pug를 뷰엔진이라고 설정
+app.use(logger); //app.use(morgan("dev"))라고 사용해도 같음
 app.use("/", globalRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
@@ -20,3 +24,18 @@ const handleListening = () => {
 };
 
 app.listen(PORT, handleListening);
+
+// #5.1 Configuring Pug
+// Pug는 템플릿 엔진 => 템플릿을 이용해서 뷰를 만드는걸 돕는다
+
+//pug 사용
+//1. pug 설치(npm i pug)
+//2. pug를 뷰엔진이라고 설정(app.set("view engine","pug"))
+//3. 실제로 pug 파일을 생성한다.
+//4. res.render("view name") => 이렇게 하면 home.pug가 랜더링 된다.
+// 여기까지 했을때 에러발생원인을  console.log("process.cwd : ", process.cwd()); => 현재 작업 디렉토리 위치를 알려줌
+// wetube안에 있는 package.json에서 node.js를 실행하고 있기 때문에(이곳이 현재 디렉토리란 의미)
+// server.js가 있는 src폴더가 아니라 wetube파일이 현재 작업중인 파일 위치로 잡히게 된다.(즉, 현재작업디렉토리는 node.js를 실행하는 디렉토리다 node.js를 실행하는건 package.json이므로 package.json이 있는곳이 현재 디렉토리라는 의미)
+//
+
+//
