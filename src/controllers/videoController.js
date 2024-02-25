998fafeb04ffed7bad7069b6110b5b5e58741577
Hyeonju Kim <hjk92go@gmail.com>
2024-02-25 17:14:23 +0900

    #5.0 Returning HTML

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,15 +1,19 @@
 //global router
-export const trending = (req, res) => res.send("Home Page Videos");
+export const trending = (req, res) =>
+  res.send(`<!DOCTYPE html><html lang='ko'><head><title>Wetube</title></head>
+<body><h1>HOME</h1></body></html><footer>&copy; 2021 Wetube- All rights reserved</footer>`);
 export const search = (req, res) => res.send("search");
 
 //video router
 export const see = (req, res) => {
   console.log(req.params);
-  return res.send(`Watch Video #${req.params.id}`);
+  res.send(`<!DOCTYPE html><html lang='ko'><head><title>Wetube</title></head>
+<body><h1>Watch Video #${req.params.id}</h1></body></html><footer>&copy; 2021 Wetube- All rights reserved</footer>`);
 };
 export const edit = (req, res) => {
   console.log(req.params);
-  return res.send("Edit");
+  return res.send(`<!DOCTYPE html><html lang='ko'><head><title>Wetube</title></head>
+  <body><h1>Edit Video #${req.params.id}</h1></body></html><footer>&copy; 2021 Wetube- All rights reserved</footer>`);
 };
 export const upload = (req, res) => res.send("upload");
 export const deleteVideo = (req, res) => {
