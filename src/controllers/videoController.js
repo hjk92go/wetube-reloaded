67f962c6a8b11747bf87f52d8a495452322b6f02
Hyeonju Kim <hjk92go@gmail.com>
2024-02-25 18:29:01 +0900

    #5.2 Partials

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -6,13 +6,11 @@ export const search = (req, res) => res.send("search");
 //video router
 export const see = (req, res) => {
   console.log(req.params);
-  res.send(`<!DOCTYPE html><html lang='ko'><head><title>Wetube</title></head>
-<body><h1>Watch Video #${req.params.id}</h1></body></html><footer>&copy; 2021 Wetube- All rights reserved</footer>`);
+  res.render("watch");
 };
 export const edit = (req, res) => {
   console.log(req.params);
-  return res.send(`<!DOCTYPE html><html lang='ko'><head><title>Wetube</title></head>
-  <body><h1>Edit Video #${req.params.id}</h1></body></html><footer>&copy; 2021 Wetube- All rights reserved</footer>`);
+  return res.render("edit");
 };
 export const upload = (req, res) => res.send("upload");
 export const deleteVideo = (req, res) => {
--- src/server.js
+++ src/server.js
@@ -14,6 +14,8 @@ const app = express();
 const logger = morgan("dev");
 
 app.set("view engine", "pug"); //pug를 뷰엔진이라고 설정
+app.set("views", process.cwd() + "/src/views"); //디폴트값 수정
+
 app.use(logger); //app.use(morgan("dev"))라고 사용해도 같음
 app.use("/", globalRouter);
 app.use("/users", userRouter);
@@ -36,6 +38,24 @@ app.listen(PORT, handleListening);
 // 여기까지 했을때 에러발생원인을  console.log("process.cwd : ", process.cwd()); => 현재 작업 디렉토리 위치를 알려줌
 // wetube안에 있는 package.json에서 node.js를 실행하고 있기 때문에(이곳이 현재 디렉토리란 의미)
 // server.js가 있는 src폴더가 아니라 wetube파일이 현재 작업중인 파일 위치로 잡히게 된다.(즉, 현재작업디렉토리는 node.js를 실행하는 디렉토리다 node.js를 실행하는건 package.json이므로 package.json이 있는곳이 현재 디렉토리라는 의미)
-//
+// "현재 작업 디렉토리 + views폴더" 가 되어야 에러 X (디폴트값)
 
-//
+// #5.2 Partials
+// 위의 에러를 고치는 방법은 디폴트 값을 바꿔주는것이다.
+// 해결 방법_1) views폴더를 src밖으로 꺼내기(비추)
+// 해결 방법_2) 디폴트값 수정 - app.set("views", process.cwd()+"/src/views")
+
+// pug 중복되는 부분 => views폴더 안에 partials 폴더를 만들어서 footer.pug
+// 를 만들어서 include 해주면 중복으로 쓸 필요가 없어짐
+// include 마저 중복으로 쓰기 싫다면, => 5.3에서
+
+// pug의 장점
+// 1) html를 깔끔하게 작성하도록 해줌
+// 2) 우리의 html에 js를 포함시킬 수 있다.(사용할 수 있음)
+// 3) 반복하지 않아도 한파일로 모든 템플릿 이용 가능
+
+/**
+ * 헤더에서 Express를 없애는 방법:
+ * app.disable('x-powered-by');
+ *
+ */
